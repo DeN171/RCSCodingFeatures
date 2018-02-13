744aaec2056a45690fe3392dc32dc8c6eb506c6a
DeN171 <moj18@inbox.lv>
2018-02-13 21:36:20 +0200

    created my ello app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayHelloToUser();
+            VardsGads();
+        }
+
+
+        static void SayHelloToUser()
+        {
+            Console.WriteLine("Ievadi savu vardu: ");
+            string usersName;
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien, " + usersName + "!");
+            Console.ReadLine();
+        }
+
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        static void VardsGads()
+        {
+            Console.WriteLine("Ievadi savu vardu: ");
+            string usersName;
+            string vecums;
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien, " + usersName + "!");
+            Console.WriteLine("Ievadi savu vecumu: ");           
+            vecums = Console.ReadLine();
+            Console.WriteLine("Tavs vecums: " + vecums);
+            Console.ReadLine();
+        }
+    }
+}
